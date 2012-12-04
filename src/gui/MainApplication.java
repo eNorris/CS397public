e170f7b63f3a1b97a87eb06e05b1d6cb373a0d85
eNorris <812xsi@gmail.com>
2012-12-03 23:38:54 -0600

    Fixed indentation

--- src/gui/MainApplication.java
+++ src/gui/MainApplication.java
@@ -29,18 +29,18 @@ public class MainApplication extends JFrame{
 	
 	public static void main(String[] args){
 		System.out.print("Debug Console...\n");
-		
+
 		World.dbc = new DBUtil();
 		if(!World.dbc.Connect()){
 			JFrame frame = new JFrame();
 			JOptionPane.showMessageDialog(frame,
-				    "Could not connect to database: " + DBUtil.DBPath + DBUtil.DBName + "\nIs the application already running?",
-				    "Database Connection Error",
-				    JOptionPane.ERROR_MESSAGE);
+					"Could not connect to database: " + DBUtil.DBPath + DBUtil.DBName + "\nIs the application already running?",
+					"Database Connection Error",
+					JOptionPane.ERROR_MESSAGE);
 			return;
 		}
 		System.out.print("Connected to database: " + DBUtil.DBPath + DBUtil.DBName + "\n\n");
-		
+
 		JFrame frame = new JFrame();
 		
 		Core core = new Core();
