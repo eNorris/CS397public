f2c288c2043dca26bb2d51b45a340f631da9b007
eNorris <812xsi@gmail.com>
2012-12-06 16:29:27 -0600

    Launched sql doer

--- src/gui/ExecSQL.java
+++ src/gui/ExecSQL.java
@@ -3,24 +3,27 @@ package gui;
 import java.io.File;
 
 import util.Util;
+import util.World;
 
 public class ExecSQL implements Runnable{
 	public void run(){
-		File lockfile = new File(Util.relPath("/data/sqllock"));
+		File lockfile = new File(Util.relPath("/scripts/sqllock"));
 		
 		while(lockfile.exists()){
 			try {
 				Thread.sleep(5000);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
 		
-		// FIXME - do stuff
-		File sqlExecFile = new File(Util.relPath("/data/query.sql"));
+		File sqlExecFile = new File(Util.relPath("/scripts/sql.sql"));
 		if(!sqlExecFile.exists())
 			return;
+		
+		if(!World.dbc.ExecuteFile(sqlExecFile.getAbsolutePath())){
+			System.out.print("Failed to execute sql file: " + sqlExecFile.getAbsolutePath() + "\n");
+		}
 		
 		
 	}
